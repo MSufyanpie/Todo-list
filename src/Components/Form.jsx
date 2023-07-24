f5f5d55ddaa0a51d188f4d4b466a6fb8364591fe
Muhammad <muhammad.sufyan@piecyfer.com>
2023-07-24 18:03:26 +0500

    files added

---
+++ src/Components/Form.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import './Form.css'
+ class Form extends Component {
+  constructor(props){
+  super(props)
+  this.state={
+    TaskList:[],
+    task:'',
+  }
+   this.HandleChange=(event)=>{
+     const {value}=event.target
+     this.setState({
+      task:value
+     });  
+  
+    };
+     this.HandleSubmit=(event)=>{
+    event.PreventDefault();
+    const {task}=this.state
+    };
+   
+    
+  
+    this.setState((prevProp)=>({
+      TaskList:[...prevProp.TaskList,task],
+      task:'',
+    }));
+  };
+  
+  render() {
+     const{TaskList,task}=this.state
+    return (
+      <div className='Form'>
+    <form  className='Todo-form' onSubmit={this.HandleSubmit}>
+    <label className='form-label'>Enter your Task</label><br></br>
+    <input id='form-input' name='task' type='text' value={task} onChange={this.HandleChange} ></input><br></br>
+    <button  className='submit-btn' type='submit'>Add Task</button>
+    
+    </form>
+
+      </div>
+    )
+  }
+}
+export default Form
