dcc926e9a631fdeac6218f27752cf24e796c6094
Muhammad <muhammad.sufyan@piecyfer.com>
2023-07-26 10:37:28 +0500

    changes in code

--- src/Components/EditForm.jsx
+++ src/Components/EditForm.jsx
@@ -1,43 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class EditForm extends Component {
-    
-    constructor(props){
-        super(props)
-        this.state={
-            tasktoEdit:this.props.task
-        }
-    }
-    // handlecancel=()=>{
-    //     this.props.HandleCancel()
-    // }
-    // handlesave=()=>{
-    //     this.props.HandleSave()
-    // }
-    HandleChange=(event)=>{
-        const {value}=event.target.value
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasktoEdit: this.props.task,
+    };
+  }
+
+  HandleChange = (event) => {
+    const { value } = event.target;
     this.setState({
-        tasktoEdit:value,
-    })
-     
-    }
+      tasktoEdit: value,
+    });
+  };
+
   render() {
-    
-    // const tasktoEdit=this.props
     return (
-        <div className='flex-container'>
-        
-      <form  className='Todo-form'>
-      <label className='form-label'><b>Enter your Task Description</b></label><br></br>
-      <input ref={(input) => (this.taskInput = input)} id='form-input' name='task' type='text' value={this.state.tasktoEdit} onChange={this.HandleChange}></input><br></br>
-      <button  className='save-btn' type='submit' onClick={this.handlesave}>Save</button>
-      <button  className='cancel-btn' type='submit' onClick={this.handlecancel}>Cancel</button>
-
-
-      
-      </form>
+      <div className="flex-container">
+        <form className="Todo-form">
+          <label className="form-label">
+            <b>Enter your Task Description</b>
+          </label>
+          <br></br>
+          <input
+            ref={(input) => (this.taskInput = input)}
+            id="form-input"
+            name="task"
+            type="text"
+            value={this.state.tasktoEdit}
+            onChange={this.HandleChange}
+          ></input>
+          <br></br>
+          <button className="save-btn" type="button" onClick={this.props.HandleSave}>
+            Save
+          </button>
+          <button className="cancel-btn" type="button" onClick={this.props.HandleCancel}>
+            Cancel
+          </button>
+        </form>
       </div>
-        
-    )
+    );
   }
 }
